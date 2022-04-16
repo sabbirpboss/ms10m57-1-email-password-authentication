c45dcfd77e0267ceb2c400b3303586137280fadd
sabbirpboss <sabbir.ah799@gmail.com>
2022-04-16 11:16:04 +0600

    update and working on

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { getAuth } from "firebase/auth";
+import "bootstrap/dist/css/bootstrap.min.css";
+import banner from "./images/Flickr.jpeg";
+import user from "./images/login-user.jpeg";
+import app from "./firebase.init";
+import Form from "react-bootstrap/Form";
+import { Button } from "react-bootstrap";
+import { useState } from "react";
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
 
 function App() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailBlur = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordBlur = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    console.log("submitted", email, password);
+    event.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-info container rounded mt-4">
+      <div className="user-img">
+        <img src={user} alt="" />
+      </div>
+      <div className="banner-img">
+        <img src={banner} alt="" />
+      </div>
+
+      <div className="registration w-50 mx-auto">
+        <h2 className="text-success">Stay Connected!!!</h2>
+        <Form onSubmit={handleFormSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDb2pPfpw--qTH7s4ssXGZzfb5lnljF9vs",
+  authDomain: "email-password-auth-ea4fe.firebaseapp.com",
+  projectId: "email-password-auth-ea4fe",
+  storageBucket: "email-password-auth-ea4fe.appspot.com",
+  messagingSenderId: "200234749675",
+  appId: "1:200234749675:web:7054423c5ef5830c7178ae"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
